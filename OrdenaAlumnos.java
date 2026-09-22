131d089197d85ee2cd828014ed92228ecce38c00
Erick Barrena Gonzalez <erickodayar89@gmail.com>
2026-09-21 18:44:40 -0600

    Ejercicio Ordena a los alumnos completo

--- OrdenaAlumnos.java
+++ OrdenaAlumnos.java
@@ -1,17 +1,37 @@
 import java.util.Scanner;
 
 public class OrdenaAlumnos {
-    public static void main(String [] args){
+    public static void main(String [] args) {
         Scanner sc = new Scanner(System.in);
 
         int numAlumnos = sc.nextInt();
-        int [] calificaciones = new int [numAlumnos];
+        int[] calificaciones = new int[numAlumnos];
 
-        int i=0;
-        while (i<numAlumnos){
-        calificaciones[i] = sc.nextInt();
+        int i = 0;
+        while (i < numAlumnos) {
+            calificaciones[i] = sc.nextInt();
             i++;
         }
-        System.out.print(calificaciones.length);
+        i = 0;
+        while (i < numAlumnos - 1) {
+            int j = i + 1;
+            while (j < numAlumnos) {
+                if (calificaciones[i] < calificaciones[j]) {
+                    int auxiliar = calificaciones[i];
+                    calificaciones[i] = calificaciones[j];
+                    calificaciones[j] = auxiliar;
+                }
+                j++;
+            }
+            i++;
+        }
+        i = 0;
+        while (i < numAlumnos) {
+            if (i > 0) {
+                System.out.print(" ");
+            }
+                System.out.print(calificaciones[i]);
+            i++;
+        }
     }
 }
